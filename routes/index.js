bff2f02e309a07621d7f3fb9c22996b9bad705fb
Rainbowbox <twentyfivehours@163.com>
2018-11-26 23:24:32 +0800

    Adding vue and socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var Vue = require('vue');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
